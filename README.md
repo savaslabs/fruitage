9d38f39287aa8e2dd65fc5e00fc58a18d6ab59da
Kosta Harlan <kosta@fastmail.com>
2017-02-19 21:54:53 -0500

    Add a README and LICENSE, wrap up the command

---
+++ README.md
@@ -0,0 +1,23 @@
+# Siros
+
+Backup all of your Harvest time entry data to CSV.
+
+## Configuration
+
+Set the following environment variables:
+
+- `HARVEST_PASSWORD`
+- `HARVEST_EMAIL`
+- `HARVEST_ACCOUNT`
+
+## Usage
+
+`docker run --rm -it -v $(pwd):/app --env-file=.env savaslabs/siros backup {filename}`
+
+Or:
+
+`php siros.php backup {filename}`
+
+If you run without an argument, the CSV will be called `data.csv`.
+
+The exported entries will be in slightly different order than what is provided by Harvest when you click "Export all data", because we sort time entries based on the "spent at" field, which is only granular to a particular day, whereas Harvest likely sorts their time entries by second or minute (or possibly the "created at" field).
--- src/Siros/Console/Command/BackupCommand.php
+++ src/Siros/Console/Command/BackupCommand.php
@@ -14,14 +14,15 @@ use Carbon\Carbon;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\Encoder\CsvEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\PropertyAccess\PropertyAccess;
 
+/**
+ * Class BackupCommand
+ * @package Siros\Console\Command
+ */
 class BackupCommand extends Command {
 
     /** @var \Harvest\HarvestAPI */
     private $harvestClient;
-    /** @var array */
-    private $config;
     /** @var array */
     private $projectMap;
     /** @var array */
@@ -29,10 +30,11 @@ class BackupCommand extends Command {
     /** @var array */
     private $userMap;
     /** @var array */
-    private $clientMap;
-    /** @var array */
     private $projectToClientMap;
 
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
         $this->setName('backup')
@@ -46,10 +48,14 @@ class BackupCommand extends Command {
             ->setDescription('Backup time entries from Harvest to a CSV.');
     }
 
+    /**
+     * Populate a map of project IDs to project names, and client IDs to client names.
+     */
     protected function populateProjectMap()
     {
         $projects = $this->harvestClient->getProjects()->get('data');
         $clients = $this->harvestClient->getClients()->get('data');
+        /** @var \Harvest\Model\Result $project */
         foreach ($projects as $project) {
             $this->projectMap[$project->get('id')] = $project->get('name');
             $client_id = $project->get('client-id');
@@ -59,6 +65,9 @@ class BackupCommand extends Command {
         }
     }
 
+    /**
+     * Initialize the Harvest client.
+     */
     protected function setHarvestClient()
     {
         $this->harvestClient = new HarvestAPI();
@@ -71,62 +80,112 @@ class BackupCommand extends Command {
         }
     }
 
+    /**
+     * Populate a map of user IDs to user names.
+     */
     protected function populateUserMap()
     {
         $users = $this->harvestClient->getUsers()->get('data');
         foreach ($users as $user) {
+            /** @var \Harvest\Model\Result $user */
             $this->userMap[$user->get('id')]['first_name'] = $user->get('first-name');
             $this->userMap[$user->get('id')]['last_name'] = $user->get('last-name');
         }
     }
 
+    /**
+     * Populate a map of task IDs to task names.
+     */
     protected function populateTaskMap()
     {
         $tasks = $this->harvestClient->getTasks()->get('data');
         foreach ($tasks as $task) {
+            /** @var \Harvest\Model\Result $task */
             $this->taskMap[$task->get('id')] = $task->get('name');
         }
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        // Initialize client.
         $this->setHarvestClient();
 
+        // Set up our file.
         $fs = new Filesystem();
         $fs->touch($input->getArgument('file'));
 
+        // Initialize output.
         $io = new SymfonyStyle($input, $output);
         $io->title('Backing up Harvest entries to CSV');
 
-        $to = Carbon::create()->format('Ymd');
+        // Set up the date range for retrieving project entries.
         // From data is an arbitrary old date.
+        $to = Carbon::create()->format('Ymd');
         $range = new Range('20100101', $to);
+
         $io->note('Populating project map.');
         $this->populateProjectMap();
+
         $io->note('Populating user map.');
         $this->populateUserMap();
+
         $io->note('Populating task map.');
         $this->populateTaskMap();
+
         $project_ids = array_keys($this->projectMap);
+
+        // Retrieve data for projects.
+        $io->section(sprintf('Retrieving data for %d projects', count($project_ids)));
         $io->progressStart(count($project_ids));
         $time_entries = [];
+        $entries = [];
         // Get all Harvest projects.
         foreach ($project_ids as $id) {
             // Get all time entries for each project.
-            $entries = $this->harvestClient->getProjectEntries($id, $range)->get('data');
-            foreach ($entries as $entry) {
-                // Format time entries.
-                $time_entries[] = $this->formatEntry($entry);
+            $project_entries = $this->harvestClient->getProjectEntries($id, $range)->get('data');
+            foreach ($project_entries as $entry) {
+                $entries[] = $entry;
             }
             $io->progressAdvance();
         }
         $io->progressFinish();
+
+        // Format and sort the time entries.
+        $io->section(sprintf('Formatting and sorting %d time entries', count($entries)));
+        $io->progressStart(count($entries));
+        foreach ($entries as $entry) {
+            // Format time entries.
+            $time_entries[] = $this->formatEntry($entry);
+            $io->progressAdvance();
+        }
+        $io->progressFinish();
+        // Sort by date.
+        usort($time_entries, function ($a, $b) {
+            $date_a = Carbon::parse($a['Date']);
+            $date_b = Carbon::parse($b['Date']);
+            if ($date_a->eq($date_b)) {
+                return 0;
+            }
+            return ($date_a->lessThan($date_b)) ? -1 : 1;
+        });
+
+        // Save the CSV.
         $serializer = new Serializer([new ObjectNormalizer()], [new CsvEncoder()]);
         $fs->dumpFile($input->getArgument('file'), $serializer->encode($time_entries, 'csv'));
+        $io->success(sprintf('Wrote %d entries to %s', count($time_entries), $input->getArgument('file')));
 
         return 0;
     }
 
+    /**
+     * Format an entry for writing to the CSV file.
+     *
+     * @param \Harvest\Model\Result $entry;
+     * @return array
+     */
     protected function formatEntry($entry)
     {
             return [
